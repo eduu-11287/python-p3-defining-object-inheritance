79db55205e15bcc003f3d81bc946b0bddd2177d4
Edwin <edmboche141@gmail.com>
2025-02-23 09:41:38 +0300

    initial commit

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,14 @@
+from car import Car
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "Filling up!"
+    
+audi = Vehicle(48, 4)
+print(audi.go())
